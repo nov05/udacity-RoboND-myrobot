d491e63f65b42531918428819275c5201751af6b
nov05 <215590+Nov05@users.noreply.github.com>
2025-02-13 18:11:25 -0600

    .

--- script/hello.cpp
+++ script/hello.cpp
@@ -2,17 +2,17 @@
 
 namespace gazebo
 {
-  class WorldPluginMyRobot : public WorldPlugin
-  {
-    public: WorldPluginMyRobot() : WorldPlugin()
-            {
-              printf("Hello World!\n");
-            }
+    class WorldPluginMyRobot : public WorldPlugin
+    {
+        public: WorldPluginMyRobot() : WorldPlugin()
+        {
+            printf("Hello World!\n");
+        }
 
-    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
-            {
-            }
-  };
-  GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyRobot)
+        public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+        {
+        }
+    };
+    GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyRobot)
 }
 
